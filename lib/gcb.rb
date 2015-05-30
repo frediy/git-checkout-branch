156b1877728778ed0a4252ae74678156363f45da
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2015-05-30 18:01:56 +1000

    add branch name matching and partial branch name  matching

--- lib/gcb.rb
+++ lib/gcb.rb
@@ -1,23 +1,53 @@
 require 'gcb/version'
 
 module Gcb
+  def self.root
+    File.expand_path '../..', __FILE__
+  end
+
   class BranchChanger
-    def initialize(input, git_repo_dir=Dir.pwd)
-       @git_repo_dir = git_repo_dir
+    def initialize(git_repository_directory=Dir.pwd)
+      @git_dir = "#{git_repository_directory}/.git"
     end
 
-    def execute!
-      branches
+    def change!(branch)
+      if branches.include?(branch)
+        checkout_branch(branch)
+      elsif partially_matching_branch = partially_matching_branch_from_branches_and_branch(branches, branch)
+        checkout_branch(partially_matching_branch)
+      else
+        # don't change branch
+      end
+      current_branch
     end
 
   private
 
+    #
+    # Matching
+    #
+    def partially_matching_branch_from_branches_and_branch(branches, branch)
+      branches.find { |existing_branch| existing_branch.include?(branch) }
+    end
+
+
+    #
+    # Git Queries
+    #
     def branches
       @branches ||= git('branch').split("\n").map { |branch| branch.gsub('*', '') }.map(&:strip)
     end
 
+    def current_branch
+      git('branch').match(/\* (.*)\n/)[1]
+    end
+
+    def checkout_branch(branch)
+      git("checkout #{branch}")
+    end
+
     def git(command)
-      `git --git-dir=#{@git_repo_dir}/.git #{command}`
+      `git --git-dir=#{@git_dir} #{command}`
     end
   end
 end
--- spec/lib/gcb_spec.rb
+++ spec/lib/gcb_spec.rb
@@ -2,21 +2,75 @@ require 'spec_helper'
 
 module Gcb
   describe BranchChanger do
-    let (:parser) { BranchChanger.new(input) }
-    let (:input) { '' }
+    GIT_TEST_REPO_DIRECTORY = "#{Gcb.root}/spec/fixtures/git_test_repo"
 
-    subject { parser.change! }
+    let (:branch_changer) { BranchChanger.new(GIT_TEST_REPO_DIRECTORY) }
 
-    let (:git_test_repo_directory) { './spec/fixtures/git_test_repo' }
+    let (:branches) { ['master'] }
 
     before do
-      initialize_git_repo(git_test_repo_directory)
+      initialize_repo
+      add_branches(branches)
     end
 
-    it { is_expected.to eq false }
+    after do
+      FileUtils.rm_rf(GIT_TEST_REPO_DIRECTORY)
+    end
 
-    def initialize_git_repo(dir)
-      `git init #{dir}`
+    describe "#change!" do
+      let (:branches) { ['master'] }
+      let (:input) { 'master' }
+
+      before do
+        branch_changer.change!(input)
+      end
+
+
+      describe 'switch to exact branch name' do
+        let (:branches) { ['master', 'new-branch'] }
+
+        let (:input) { 'new-branch' }
+        let (:expected_branch) { 'new-branch' }
+
+        specify { expect(current_branch).to eq expected_branch }
+      end
+
+      describe 'switch to partial branch name' do
+        let (:branches) { ['master', 'new-branch'] }
+
+        let (:input) { 'new' }
+        let (:expected_branch) { 'new-branch' }
+
+        specify { expect(current_branch).to eq expected_branch }
+      end
+    end
+
+    def initialize_repo
+      `git init #{GIT_TEST_REPO_DIRECTORY}`
+      `touch #{GIT_TEST_REPO_DIRECTORY}/file`
+      git("add #{GIT_TEST_REPO_DIRECTORY}")
+      git("commit -m 'initial'")
+    end
+
+    def add_branches(branches)
+      branches.each do |branch|
+        next if branch == 'master'
+        add_branch(branch)
+      end
+    end
+
+    def add_branch(branch_name)
+      previous_branch = current_branch
+      git("checkout -b #{branch_name}")
+      git("checkout #{previous_branch}")
+    end
+
+    def current_branch
+      git('branch').match(/\* (.*)\n/)[1]
+    end
+
+    def git(command)
+      `git --git-dir=#{GIT_TEST_REPO_DIRECTORY}/.git #{command}`
     end
   end
 end
